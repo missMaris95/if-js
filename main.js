707fb4cf2c0893e2628c4aa5afd0728b27bbaed2
Mariya Hurlenia <sasinovichmaria@gmail.com>
2023-11-09 23:31:32 +0300

    homework 2

--- main.js
+++ main.js
@@ -1,3 +1,64 @@
-const text = "hello world";
+//6
+let user = 'John Doe' ;
+const student = 'Maris' ;
 
-console.log(text);
+user = student ;
+// в переменнной юзер теперь данные стьюдентс
+
+console.log(user);
+console.log(student);
+
+//7
+let test = 1 ;
+
+console.log( test += 1 );
+
+const num = '1' ;
+console.log( test + num );
+// мы просуммировали числовой и строчный оператор. в итоге они стали строчными
+
+console.log( test );
+
+console.log( test -= 1 );
+//??????
+
+test = true;
+
+console.log(test);
+
+// const name = 'Якуб ';
+// const surname = 'Колас';
+//
+// console.log(name + surname);
+
+//дано
+// const hello = 'привтсвую, ';
+// const name = 'Янка ';
+// const surname = 'Купала';
+//
+// console.log(hello + name +surname);
+// //вывести в консоль 'Прывiтанне, Янка Купала!'
+
+//дано
+// const radius = 5;
+//
+// console.log(2 * radius * 3.14);
+//вывести длину окружности (2πr)
+
+//дано
+// const a = 5;
+// const b = 11;
+//
+// console.log( (a + b) / 2 );
+//вывести среденее значение двух чисел
+
+// let status = 140;
+// if ( status >= 18 ) {
+//   console.log( 'Привет' );
+// }
+//
+//
+// console.log( 'Приветствую' );
+//если пользователь введет возраст до 18 - вывести: привет
+//если пользователь введет возраст от 18 - вывести: здравствуйте
+
