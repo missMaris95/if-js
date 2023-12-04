3d08bfe18cd350e520ef8d70e1ac136a09b1fb7e
Mariya Hurlenia <sasinovichmaria@gmail.com>
2023-12-04 19:02:31 +0300

    added homework for click

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,22 +4,46 @@ function sum(n1) {
   if (Number.isNaN(n1)) {
     console.log('вы ввели не число'); //не выводит
   }
-  console.log(n1);
-  return (n2) => {
-    console.log(n1 + n2);
-  };
+  return (n2) => n1 + n2;
 }
 
 console.log(sum(5)(2));
 
-// const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
-//
-// const chengeColor = function (){
-//   for (let i = 0; i<colors.length; i++){
-//
-//   }
-// };
-//
+//*********************************************************************************
+
+
+const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+const text1 = document.getElementById('text1');
+const text2 = document.getElementById('text2');
+const text3 = document.getElementById('text3');
+
+function nextColor(){
+    for (let i = 0; i<colors.length; i++){
+        return event => {
+            const e = event.target;
+            e.style.color = colors[i];
+            if (i >= colors.length){
+                return i=0;
+            }
+            return [i++];
+        };
+    }
+}
+
+text1.addEventListener('click',nextColor());
+text2.addEventListener('click',nextColor());
+text3.addEventListener('click',nextColor());
+
+
+
+
+
+
+
+
+
+
+
 // const text1 = document.getElementById('text1');
 // const text2 = document.getElementById('text2');
 // const text3 = document.getElementById('text3');
