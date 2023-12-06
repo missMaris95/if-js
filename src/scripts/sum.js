465e0382be92db0249f1d0d3fca2f468fb8d60ec
Mariya Hurlenia <sasinovichmaria@gmail.com>
2023-12-06 13:50:50 +0300

    Added CORS

--- src/scripts/sum.js
+++
@@ -1,6 +0,0 @@
-function sum(n1) {
-  return (n2) => n1 + n2;
-}
-
-console.log(sum(5)(2));
-module.exports = sum();
--- src/tests/sum.test.js
+++ src/tests/sum.test.js
@@ -1,6 +1,8 @@
-const sum = require('../scripts/sum');
+function sum(n1) {
+  return (n2) => n1 + n2;
+}
 
 test('sum should return sum of two values', () => {
-  expect(5 + 2).toBe(7);//почему не работает вариант с  expect(sum(5 ,2)).toBe(7);
+  expect(sum(5)(2)).toBe(7); //почему не работает вариант с  expect(sum(5 ,2)).toBe(7);
 });
 module.exports = { sum };
