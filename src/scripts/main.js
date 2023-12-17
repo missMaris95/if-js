7d7881bb3f134e704b814de826252085fcb283a2
Mariya Hurlenia <sasinovichmaria@gmail.com>
2023-12-17 23:00:30 +0300

    Added array with new date

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,36 +5,45 @@
 // и приводить ее к поставленному в задании формату.
 
 //Вариант 1 через регулярные выражения
-//   const data = '2025-11-05';
-// //  const re =/[(\d)+\b-]+/; //без группы
-// //   const re = /(\d+)\b-(\d+)\b-(\d+)/ //with group
-//   const re = /(?<year>\d+)\D(?<month>\d+)\D(?<day>\d+)/; //with name group
-//   const revers = data.replace(re,'$<day>.$<month>.$<year>');
-//
-// console.log(revers);
+  const data1 = '2025-11-05';
+//  const re =/[(\d)+\b-]+/; //без группы
+//   const re = /(\d+)\b-(\d+)\b-(\d+)/ //with group
+  const re = /(?<year>\d+)\D(?<month>\d+)\D(?<day>\d+)/; //with name group
+  const revers = data1.replace(re,'$<day>.$<month>.$<year>');
 
+console.log(revers);
 
 //Вариант 2 через методы строк
-// const data = '2025-11-05';
-// const year = data.slice(0,4);
-// const month = data.slice(5,7);
-// const day = data.slice(-2);
-//
-// const newData = day+ '.' +month+ '.' +year;
-// console.log(newData);
+const data2 = '2025-11-05';
+const reversDate = function (str){
+    return str.split('-').reverse().join('.');
+};
+
+console.log(reversDate(data2));
 
 
-//****************
-// const array = data.split('');
-// console.log(array);
-// const notDefis = data.replace('-','');
-// console.log(notDefis);
-// const reversArray = array.reverse();
-// console.log(reversArray);
-// const reversArrToString = reversArray.toString();
-// console.log(reversArrToString);
-// const reversDate = reversArrToString.replace(',','');
-// console.log(reversDate);
+
+// //Вариант 3
+const data3 = '2025-11-05';
+function revDate(str){
+    const arr = str.split('-');
+    const arrRevers = arr.reverse();
+    return  arrRevers.join('.');
+}
+
+console.log(revDate(data3));
+
+//вариант 4
+const data4 = '2025-11-05';
+function reversData(str){
+    const year = str.slice(0,4);
+    const month = str.slice(5,7);
+    const day = str.slice(-2);
+    return  day+ '.' +month+ '.' +year;
+}
+
+console.log(reversData(data4));
+
 
 
 //Поиск объектов размещения:
@@ -43,146 +52,66 @@
 // по полученной строке найдите все совпадения в массиве;
 // верните список строк в формате: страна, город, отель.
 
-// const data = [
-//   {
-//     country: 'Russia',
-//     city: 'Saint Petersburg',
-//     hotel: 'Hotel Leopold',
-//   },
-//   {
-//     country: 'Spain',
-//     city: 'Santa Cruz de Tenerife',
-//     hotel: 'Apartment Sunshine',
-//   },
-//   {
-//     country: 'Slowakia',
-//     city: 'Vysokie Tatry',
-//     hotel: 'Villa Kunerad',
-//   },
-//   {
-//     country: 'Germany',
-//     city: 'Berlin',
-//     hotel: 'Hostel Friendship',
-//   },
-//   {
-//     country: 'Indonesia',
-//     city: 'Bali',
-//     hotel: 'Ubud Bali Resort&SPA',
-//   },
-//   {
-//     country: 'Netherlands',
-//     city: 'Rotterdam',
-//     hotel: 'King Kong Hostel',
-//   },
-//   {
-//     country: 'Marocco',
-//     city: 'Ourika',
-//     hotel: 'Rokoko Hotel',
-//   },
-//   {
-//     country: 'Germany',
-//     city: 'Berlin',
-//     hotel: 'Hotel Rehberge Berlin Mitte',
-//   },
-// ];
 
 
 
+const data = [
+  {
+    country: 'Russia',
+    city: 'Saint Petersburg',
+    hotel: 'Hotel Leopold',
+  },
+  {
+    country: 'Spain',
+    city: 'Santa Cruz de Tenerife',
+    hotel: 'Apartment Sunshine',
+  },
+  {
+    country: 'Slowakia',
+    city: 'Vysokie Tatry',
+    hotel: 'Villa Kunerad',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hostel Friendship',
+  },
+  {
+    country: 'Indonesia',
+    city: 'Bali',
+    hotel: 'Ubud Bali Resort&SPA',
+  },
+  {
+    country: 'Netherlands',
+    city: 'Rotterdam',
+    hotel: 'King Kong Hostel',
+  },
+  {
+    country: 'Marocco',
+    city: 'Ourika',
+    hotel: 'Rokoko Hotel',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hotel Rehberge Berlin Mitte',
+  },
+];
 
 
 
+const word = 'Germany';
+function searchStr(str){
+    const result = [];
+    for (let i=0; i<str.length; i++){
+        if (data[i].country === word || data[i].city === word || data[i].hotel === word){
+            result.push(data[i].country+' '+ data[i].city+' '+ data[i].hotel);
+        }
+    }
+    return result;
+}
+
+console.log(searchStr(data));
 
 
 
-
-// const re =/\D/;
-// const str ='string';
-// console.log(re.exec(str));
-
-
-// Напишите функцию sum, которая возвращает сумму чисел следующим образом
-//
-// function sum(n1) {
-//   if (Number.isNaN(n1)) {
-//     console.log('вы ввели не число'); //не выводит
-//   }
-//   return (n2) => n1 + n2;
-// }
-//
-// console.log(sum(5)(2));
-// //
-// // //*********************************************************************************
-//
-// const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
-// const text1 = document.getElementById('text1');
-// const text2 = document.getElementById('text2');
-// const text3 = document.getElementById('text3');
-//
-// const nextColor = () => {
-//   let i = 0;
-//   return function (event) {
-//     event.target.style.color = colors[i];
-//     i++;
-//     if (i > colors.length) {
-//       i = 0;
-//     }
-//   };
-// };
-//
-// const chengeColor1 = nextColor();
-// const chengeColor2 = nextColor();
-// const chengeColor3 = nextColor();
-//
-// text1.addEventListener('click', chengeColor1);
-// text2.addEventListener('click', chengeColor2);
-// text3.addEventListener('click', chengeColor3);
-//
-// //******************
-// function getTypeNumner(num) {
-//   if (num % 2 === 0) {
-//     return 'Четное число:' + num;
-//   }
-//   return 'Nechетное число:' + num;
-// }
-// console.log(getTypeNumner(3));
-//
-// //************************
-// function findSmallestInt(...nums) {
-//   let smollestNum = nums[0];
-//   for (let i = 0; i < nums.length; i++) {
-//     if (nums[i] < smollestNum) {
-//       smollestNum = nums[i];
-//     }
-//   }
-//   return smollestNum;
-// }
-//
-// console.log(findSmallestInt(10, 5, 4, 18, 6));
-//
-// //*****************
-// function countVowels(str) {
-//   const vowels = ['a', 'e', 'i', 'o', 'u'];
-//   let glasn = 0;
-//   for (let i = 0; i < str.length; i++) {
-//     for (let j = 0; j < str.length; j++) {
-//       if (str[i] === vowels[j]) {
-//         glasn++;
-//       }
-//     }
-//   }
-//   console.log(glasn);
-//   return glasn;
-// }
-//
-// countVowels('street');
-//
-// //*****
-// const summ = (n) => {
-//   if (n !== 1) {
-//     return n * summ(n - 1);
-//   } else {
-//     return 1;
-//   }
-// };
-//
-// console.log(summ(5));
