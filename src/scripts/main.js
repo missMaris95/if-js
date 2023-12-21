6633b159a3269b343249d4de53bbab46e13313ea
Mariya Hurlenia <sasinovichmaria@gmail.com>
2023-12-21 10:39:52 +0300

    added method forEach

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,110 +1,325 @@
-// Преобразование формата даты:
-//     в переменной date лежит дата в формате '2020-11-26';
-// преобразуйте эту дату в формат '26.11.2020';
-// функция должна быть универсальной, т.е. принимать любую дату
-// и приводить ее к поставленному в задании формату.
+//5
+const palindrom = (str) => str.split('').reverse().join('') === str;
 
-//Вариант 1 через регулярные выражения
-const data1 = '2025-11-05';
-//  const re =/[(\d)+\b-]+/; //без группы
-//   const re = /(\d+)\b-(\d+)\b-(\d+)/ //with group
-const re = /(?<year>\d+)\D(?<month>\d+)\D(?<day>\d+)/; //with name group
-const revers = data1.replace(re, '$<day>.$<month>.$<year>');
+console.log(palindrom('шалаш'));
 
-console.log(revers);
-
-//Вариант 2 через методы строк
-const data2 = '2025-11-05';
-const reversDate = function (str) {
-  return str.split('-').reverse().join('.');
-};
-
-console.log(reversDate(data2));
-
-// //Вариант 3
-const data3 = '2025-11-05';
-function revDate(str) {
-  const arr = str.split('-');
-  const arrRevers = arr.reverse();
-  return arrRevers.join('.');
-}
-
-console.log(revDate(data3));
-
-//вариант 4
-const data4 = '2025-11-05';
-function reversData(str) {
-  const year = str.slice(0, 4);
-  const month = str.slice(5, 7);
-  const day = str.slice(-2);
-  return day + '.' + month + '.' + year;
-}
-
-console.log(reversData(data4));
-
-//Поиск объектов размещения:
-// дан массив;
-// напишите функцию поиска, которая будет принимать строку;
-// по полученной строке найдите все совпадения в массиве;
-// верните список строк в формате: страна, город, отель.
-
-const data = [
+//6
+const hotels = [
   {
-    country: 'Russia',
+    name: 'Hotel Leopold',
     city: 'Saint Petersburg',
-    hotel: 'Hotel Leopold',
+    country: 'Russia',
   },
   {
-    country: 'Spain',
+    name: 'Apartment Sunshine',
     city: 'Santa Cruz de Tenerife',
-    hotel: 'Apartment Sunshine',
+    country: 'Spain',
   },
   {
-    country: 'Slowakia',
+    name: 'Villa Kunerad',
     city: 'Vysokie Tatry',
-    hotel: 'Villa Kunerad',
+    country: 'Slowakia',
   },
   {
-    country: 'Germany',
+    name: 'Hostel Friendship',
     city: 'Berlin',
-    hotel: 'Hostel Friendship',
+    country: 'Germany',
   },
   {
+    name: 'Radisson Blu Hotel',
+    city: 'Kyiv',
+    country: 'Ukraine',
+  },
+  {
+    name: 'Paradise Hotel',
+    city: 'Guadalupe',
+    country: 'Mexico',
+  },
+  {
+    name: 'Hotel Grindewald',
+    city: 'Interlaken',
+    country: 'Switzerland',
+  },
+  {
+    name: 'The Andaman Resort',
+    city: 'Port Dickson',
+    country: 'Malaysia',
+  },
+  {
+    name: 'Virgin Hotel',
+    city: 'Chicago',
+    country: 'USA',
+  },
+  {
+    name: 'Grand Beach Resort',
+    city: 'Dubai',
+    country: 'United Arab Emirates',
+  },
+  {
+    name: 'Shilla Stay',
+    city: 'Seoul',
+    country: 'South Korea',
+  },
+  {
+    name: 'San Firenze Suites',
+    city: 'Florence',
+    country: 'Italy',
+  },
+  {
+    name: 'The Andaman Resort',
+    city: 'Port Dickson',
+    country: 'Malaysia',
+  },
+  {
+    name: 'Black Penny Villas',
+    city: 'BTDC, Nuca Dua',
     country: 'Indonesia',
-    city: 'Bali',
-    hotel: 'Ubud Bali Resort&SPA',
   },
   {
+    name: 'Koko Hotel',
+    city: 'Tokyo',
+    country: 'Japan',
+  },
+  {
+    name: 'Ramada Plaza',
+    city: 'Istanbul',
+    country: 'Turkey',
+  },
+  {
+    name: 'Waikiki Resort Hotel',
+    city: 'Hawaii',
+    country: 'USA',
+  },
+  {
+    name: 'Puro Hotel',
+    city: 'Krakow',
+    country: 'Poland',
+  },
+  {
+    name: 'Asma Suites',
+    city: 'Santorini',
+    country: 'Greece',
+  },
+  {
+    name: 'Cityden Apartments',
+    city: 'Amsterdam',
     country: 'Netherlands',
-    city: 'Rotterdam',
-    hotel: 'King Kong Hostel',
   },
   {
-    country: 'Marocco',
-    city: 'Ourika',
-    hotel: 'Rokoko Hotel',
+    name: 'Mandarin Oriental',
+    city: 'Miami',
+    country: 'USA',
   },
   {
+    name: 'Concept Terrace Hotel',
+    city: 'Rome',
+    country: 'Italy',
+  },
+  {
+    name: 'Ponta Mar Hotel',
+    city: 'Fortaleza',
+    country: 'Brazil',
+  },
+  {
+    name: 'Four Seasons Hotel',
+    city: 'Sydney',
+    country: 'Australia',
+  },
+  {
+    name: 'Le Meridien',
+    city: 'Nice',
+    country: 'France',
+  },
+  {
+    name: 'Apart Neptun',
+    city: 'Gdansk',
+    country: 'Poland',
+  },
+  {
+    name: 'Lux Isla',
+    city: 'Ibiza',
+    country: 'Spain',
+  },
+  {
+    name: 'Nox Hostel',
+    city: 'London',
+    country: 'UK',
+  },
+  {
+    name: 'Leonardo Vienna',
+    city: 'Vienna',
+    country: 'Austria',
+  },
+  {
+    name: 'Adagio Aparthotel',
+    city: 'Edinburgh',
+    country: 'UK',
+  },
+  {
+    name: 'Steigenberger Hotel',
+    city: 'Hamburg',
     country: 'Germany',
-    city: 'Berlin',
-    hotel: 'Hotel Rehberge Berlin Mitte',
   },
 ];
 
-function searchStr(array, word) {
-  const result = [];
-  for (let i = 0; i < array.length; i++) {
-    if (
-      array[i].country === word ||
-      array[i].city === word ||
-      array[i].hotel === word
-    ) {
-      result.push(
-        array[i].country + ' ' + array[i].city + ' ' + array[i].hotel,
-      );
-    }
-  }
-  return result;
-}
 
-console.log(searchStr(data, 'Ourika'));
+//****************************************
+const  search = function(array, word){
+  const result = [];
+  hotels.forEach(i =>{
+    if (i.country===word || i.city===word || i.name===word){
+      result.push(i.country + ' ' + i.city + ' ' + i.name + ' ');
+    }
+  });
+  return result;
+};
+
+console.log(search(hotels,'Germany'));
+
+//****************************************
+// const word  = 'Germany';
+// let result = [];
+// hotels.forEach(i =>{
+//   if (i.country===word || i.city===word || i.name===word){
+//     result+= i.country + ' ' + i.city + ' ' + i.name + ',';
+//   }
+// });
+// console.log(result);
+
+//****************************************
+// function string(array, word){
+//   const result = [];
+//   for (let i = 0; i<array.length; i++){
+//     if (word === array[i].name || word === array[i].city || word === array[i].country){
+//       result.push(array[i].country+ ' ' + array[i].city+ ' ' + array[i].name);
+//     }
+//   }
+//   return result;
+// }
+//
+// console.log(string(hotels,'Germany'));
+
+
+
+// Поиск объектов размещения:
+//     дан массив;
+// напишите функцию поиска, которая будет принимать строку;
+// по полученной строке найдите все совпадения в массиве по любому из полей;
+// верните масcив строк в формате: страна, город, отель;
+// зная, как работать с массивами, сократите вашу функцию, избавившись от цикла for.
+
+//7
+
+
+
+
+// // Преобразование формата даты:
+// //     в переменной date лежит дата в формате '2020-11-26';
+// // преобразуйте эту дату в формат '26.11.2020';
+// // функция должна быть универсальной, т.е. принимать любую дату
+// // и приводить ее к поставленному в задании формату.
+//
+// //Вариант 1 через регулярные выражения
+// const data1 = '2025-11-05';
+// //  const re =/[(\d)+\b-]+/; //без группы
+// //   const re = /(\d+)\b-(\d+)\b-(\d+)/ //with group
+// const re = /(?<year>\d+)\D(?<month>\d+)\D(?<day>\d+)/; //with name group
+// const revers = data1.replace(re, '$<day>.$<month>.$<year>');
+//
+// console.log(revers);
+//
+// //Вариант 2 через методы строк
+// const data2 = '2025-11-05';
+// const reversDate = function (str) {
+//   return str.split('-').reverse().join('.');
+// };
+//
+// console.log(reversDate(data2));
+//
+// // //Вариант 3
+// const data3 = '2025-11-05';
+// function revDate(str) {
+//   const arr = str.split('-');
+//   const arrRevers = arr.reverse();
+//   return arrRevers.join('.');
+// }
+//
+// console.log(revDate(data3));
+//
+// //вариант 4
+// const data4 = '2025-11-05';
+// function reversData(str) {
+//   const year = str.slice(0, 4);
+//   const month = str.slice(5, 7);
+//   const day = str.slice(-2);
+//   return day + '.' + month + '.' + year;
+// }
+//
+// console.log(reversData(data4));
+//
+// //Поиск объектов размещения:
+// // дан массив;
+// // напишите функцию поиска, которая будет принимать строку;
+// // по полученной строке найдите все совпадения в массиве;
+// // верните список строк в формате: страна, город, отель.
+//
+// const data = [
+//   {
+//     country: 'Russia',
+//     city: 'Saint Petersburg',
+//     hotel: 'Hotel Leopold',
+//   },
+//   {
+//     country: 'Spain',
+//     city: 'Santa Cruz de Tenerife',
+//     hotel: 'Apartment Sunshine',
+//   },
+//   {
+//     country: 'Slowakia',
+//     city: 'Vysokie Tatry',
+//     hotel: 'Villa Kunerad',
+//   },
+//   {
+//     country: 'Germany',
+//     city: 'Berlin',
+//     hotel: 'Hostel Friendship',
+//   },
+//   {
+//     country: 'Indonesia',
+//     city: 'Bali',
+//     hotel: 'Ubud Bali Resort&SPA',
+//   },
+//   {
+//     country: 'Netherlands',
+//     city: 'Rotterdam',
+//     hotel: 'King Kong Hostel',
+//   },
+//   {
+//     country: 'Marocco',
+//     city: 'Ourika',
+//     hotel: 'Rokoko Hotel',
+//   },
+//   {
+//     country: 'Germany',
+//     city: 'Berlin',
+//     hotel: 'Hotel Rehberge Berlin Mitte',
+//   },
+// ];
+//
+// function searchStr(array, word) {
+//   const result = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (
+//       array[i].country === word ||
+//       array[i].city === word ||
+//       array[i].hotel === word
+//     ) {
+//       result.push(
+//         array[i].country + ' ' + array[i].city + ' ' + array[i].hotel,
+//       );
+//     }
+//   }
+//   return result;
+// }
+//
+// console.log(searchStr(data, 'Ourika'));
