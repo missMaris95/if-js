aabc20400eadd6c2a64698bba94bccc3f8a10562
Mariya Hurlenia <sasinovichmaria@gmail.com>
2024-02-11 16:13:13 +0300

    added four cards and text content

--- src/test/sum.test.js
+++ src/test/sum.test.js
@@ -1,10 +1,10 @@
 const { logPlugin } = require('@babel/preset-env/lib/debug');
 
 function sum(n1) {
-  return (n2) => n1 + n2;
+    return (n2) => n1 + n2;
 }
 
 test('sum should return sum of two values', () => {
-  expect(sum(5)(2)).toBe(7);
+    expect(sum(5)(2)).toBe(7);
 });
 module.exports = { sum };
